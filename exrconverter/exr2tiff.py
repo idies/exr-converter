06a5d4a5f464430c51039e745e8c647c73a9db3d
feingoldj <feingoldj@github.com>
2019-10-25 21:51:46 +0000

    tiff functions almost complete.

--- exrconverter/exr2tiff.py
+++ exrconverter/exr2tiff.py
@@ -1,8 +1,56 @@
-from astropy.io import fits
+from PIL import Image
 import numpy
 import OpenEXR
 import Imath
 import json
 import warnings
-from utils import * 
+from .utils import __FITS_HEADERS_ID, __get_pixeltype_from_channel, __get_exrpixel_from_channel,\
+    __change_array_type
+from .pixeltype import PixelType
 
+def convert(input_exr, output_tiff, output_pixel_type=None, verbose=True):
+    
+    exr_file = OpenEXR.InputFile(input_exr)
+    exr_header = exr_file.header()
+    tiff_headers = json.loads(exr_header[__TIFF_HEADERS_ID])
+    dw = exr_header['dataWindow']
+    image_size = (dw.max.x - dw.min.x + 1, dw.max.y - dw.min.y + 1)
+    exr_header = exr_file.header()
+    tiff_image_list = []
+    
+    for channel_idex in range(len(exr_file.header()['channels'])):
+        
+        channel_type = exr_header['channels'][str(channel_index)]
+        byte_image = exr_file.channel(str(channel_index), __get_exrpixel_from_channel(channel_type))
+        pixel_type = __get_pixeltype_from_channel(channel_type)
+        
+        if output_pixel_type is None:
+            output_pixel_type = pixel_type
+        
+        image_data = numpy.frombuffer(byte_image, dtype=pixel_type)
+        # Make sure these should be switched here
+        image_data_shape = (image_size[1], image_size[0])
+        
+        # Pretty sure TIFF supports 16 bit!!!
+        image_data = __change_array_type(image_data, output_pixel_type)
+        
+        # Might have to specify type here
+        image = Image.fromarray(image_data)
+        
+        # Not sure that this is the right thing to do here. Need to add tags to each image
+        image.tag = tiff_headers[channel_index]
+        
+        tiff_image_list.append(image)
+    
+    if len(tiff_image_list) == 0:
+        warnings.warn("No image provided.")
+        
+    elif len(tiff_image_list) == 1:
+        tiff_image_list[0].save(output_tiff, format = "tiff")
+        
+    else:
+        tiff_image_list[0].save(output_tiff, format = "tiff", append_images = tiff_image_list[1:], save_all = True)
+        
+    
+        
+            
--- exrconverter/tiff2exr.py
+++ exrconverter/tiff2exr.py
@@ -1,8 +1,52 @@
-from astropy.io import fits
+from PIL import Image
 import numpy
 import OpenEXR
 import Imath
 import json
 import warnings
-from utils import * 
+from .utils import __FITS_HEADERS_ID, __get_pixeltype_from_channel, __get_exrpixel_from_channel, __change_array_type
+from .pixeltype import PixelType
 
+def convert(input_tiff, output_exr, output_pixel_type=None, verbose=True):
+    
+    tiff_image_indexes = []
+    image_array_shape = None
+    
+    with Image.open('multipage_tiff_example.tif') as tiff:
+                
+        for index in range(tiff.n_frames):
+            tiff.seek(index)
+            
+            # This might need to be fleshed out more. 
+            if image_array_shape is None:
+                # 256 is the key for ImageWidth and 257 is the key for ImageLength
+                image_array_shape = (tiff.tag[256][0], tiff.tag[257][0])
+                tiff_image_indexes.append(index)
+            else:
+                if (tiff.tag[256], tiff.tag[257]) == image_array_shape:
+                    tiff_image_indexes.append(index)
+        
+        if len(tiff_image_indexes) == 0:
+            if verbose:
+                warnings.warn("Tiff file has no images")
+            return
+        
+        print (type(image_array_shape[1]))
+        exr_header = OpenEXR.Header(image_array_shape[1], image_array_shape[0])
+        exr_header['channels'] = {}
+        exr_data = {}
+        tiff_headers = []
+
+        for exr_index, tiff_index in enumerate(tiff_image_indexes):
+            tiff.seek(tiff_index)
+            exr_data[str(exr_index)] = __change_array_type(np.array(tiff), output_pixel_type)
+            exr_header['channels'][str(exr_index)] = __get_channel_from_pixeltype(output_pixel_type)
+            tiff_headers.append(dict(tiff.tag))
+            
+        exr_header[__TIFF_HEADERS_ID] = str.encode(json.dumps(tiff_headers))
+
+        try:
+            exr_file = OpenEXR.OutputFile(output_exr, exr_header)
+            exr_file.writePixels(exr_data)
+        finally:
+            exr_file.close()
